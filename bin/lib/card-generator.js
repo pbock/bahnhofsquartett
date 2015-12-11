6ecb1f0f13499c6cca5df30424bd81306f0a6a8a
Philipp Bock <p@philippbock.de>
2015-12-12 00:46:53 +0100

    Fix white text on white background

--- bin/lib/card-generator.js
+++ bin/lib/card-generator.js
@@ -9,7 +9,9 @@ const MARGIN = 10;
 const LINE_HEIGHT = 14;
 
 const VERKEHRSROT = [ 0, 100, 100, 10 ];
+const LICHTGRAU = [ 0, 0, 0, 20 ];
 const WHITE = [ 0, 0, 0, 0 ];
+const BLACK = [ 0, 0, 0, 100 ];
 
 function makePDF(card) {
   let doc = new PDFDocument({ size: [ WIDTH, HEIGHT ], margin: MARGIN });
@@ -17,21 +19,21 @@ function makePDF(card) {
   doc.rect(0, 0, WIDTH, HEIGHT / 2.5);
   y = HEIGHT / 2.2;
 
-  doc.fill([ 0, 0, 0, 20 ]);
+  doc.fill(LICHTGRAU);
 
   doc.moveTo(0, HEIGHT / 2.5 + 4)
     .lineTo(WIDTH, HEIGHT / 2.5 + 4)
     .lineWidth(3)
     .stroke(VERKEHRSROT);
 
-  doc.fill([ 0, 0, 0, 100 ]);
+  doc.fill(BLACK);
   doc.font(pr(__dirname, '../../src/fonts/FiraSans-Light.ttf'), 'Light');
   doc.font(pr(__dirname, '../../src/fonts/FiraSans-Book.ttf'), 'Regular');
   doc.fontSize(12);
   doc.font('Light').fill(WHITE).text(card.id, MARGIN, MARGIN, { align: 'right' });
 
   doc.fontSize(9);
-  doc.font('Regular').text(card.name, MARGIN, y);
+  doc.font('Regular').fill(BLACK).text(card.name, MARGIN, y);
   y += LINE_HEIGHT * 1.5;
 
   doc.fontSize(8);
